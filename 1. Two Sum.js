b64e519a2f83ebce7638f60d939ab258b6398a85
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-06-18 14:23:34 -0400

    Added 1. Two Sum.py; Updated

--- 1. Two Sum.js
+++ 1. Two Sum.js
@@ -9,16 +9,17 @@ var twoSum = function(nums, target) {
     }
 };
 
+
 // The overall time complexity is O(n), where n is the number of elements in the input array. 
 // 
 // The process I went through here is that I knew I could use a hashmap so I can traverse the array once, making the 
 // overall time complexity O(n). Here is the breakdown of my code:
 //     1. I start my function by initializing the hashmap as hm.
-//     2. Then, I open a for-loop to iterate through all the elements of the array. 
+//     2. Then, I open a for-loop to iterate through all the indexes of the array. 
 //     3. Then, I add an if-else statement to have two cases:
-//         a) if the compliment* value exists in the hashmap, return the two indexes whose value add up to the target.
+//         a) if the compliment* value exists in the hashmap, return the two indexes whose values add up to the target.
 //         b) if the compliment* value does not exist in the hashmap, add it to the hashmap as a value-index pair. 
-//     4. It will keep iterating the array until the value is returned since there will always be a solution. 
+//     4. It will keep iterating the array until the solution pair is returned since there will always be a solution. 
 //
 //
 // *compliment value is equivalent to target - nums[x]. 
---
+++ 1. Two Sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hm = {}
+        for x in range(len(nums)):
+            if (target - nums[x]) in hm:
+                return [x, hm[target - nums[x]]]
+            else:
+                hm[nums[x]] = x
+
+
+# The overall time complexity is O(n), where n is the number of elements in the input array. 
+# 
+# The process I went through here is that I knew I could use a hashmap so I can traverse the array once, making the 
+# overall time complexity O(n). Here is the breakdown of my code:
+#     1. I start my function by initializing the hashmap as hm.
+#     2. Then, I open a for-loop to iterate through all the indexes of the array. 
+#     3. Then, I add an if-else statement to have two cases:
+#         a) if the compliment* value exists in the hashmap, return the two indexes whose values add up to the target.
+#         b) if the compliment* value does not exist in the hashmap, add it to the hashmap as a value-index pair. 
+#     4. It will keep iterating the array until the solution pair is returned since there will always be a solution. 
+#
+#
+# *compliment value is equivalent to target - nums[x]. 
