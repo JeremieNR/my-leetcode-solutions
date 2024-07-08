bb693a5b9853cc8a495edadce12ceab25e1eabd6
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-07-08 19:10:12 -0400

    Added 231. Power of Two.js

---
+++ 231. Power of Two.js
@@ -0,0 +1,15 @@
+var isPowerOfTwo = function(n) {
+    for (let x = 0; x < 31; x++) {
+        if (2**x == n) {
+            return true;
+        }
+    }
+    return false;
+};
+
+
+// The overall time complexity is O(1). 
+// 
+// The process I went through here is that I knew I could use the maximum integer (2^32) as a loop max, making the 
+// overall time complexity O(1). I start my function by openning a for-loop, which checks if n is any power of 2. 
+// If it is, it returns true, and if the for-loop ends, it means it isn't and thus returns false.
