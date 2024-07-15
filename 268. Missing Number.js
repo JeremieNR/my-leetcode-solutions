4aeeabd9979ec40bd4b5894d2aa22b047fa586c8
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-07-15 09:16:45 -0400

    Added 268. Missing Number.js

---
+++ 268. Missing Number.js
@@ -0,0 +1,28 @@
+var missingNumber = function(nums) {
+    let maximum = 0;
+    let hm = {};
+    for (let x = 0; x < nums.length; x++) {
+        hm[nums[x]] = 0;
+        if (nums[x] > maximum) {
+            maximum = nums[x];
+        }
+    }
+    for (let x = 0; x < maximum; x++) {
+        if (hm[x] == undefined) {
+            return x;
+        }
+    }
+    return maximum + 1;
+};
+
+
+// The overall time complexity is O(n), where n is the number of elements in the input array. 
+//
+// The process I went though here is that I knew I could use a hashmap so I can traverse the array once, making the
+// overall time complexity O(n). Here is the breakdown of my code:
+//     1. I start my function by initializing the maximum int and the hashmap as hm. 
+//     2. Then I open a for-loop to index the elements of the array in the hashmap, and update the maximum int in 
+//        the array.
+//     3. Then I open another for-loop to check which element in the "full" array does not exist in the indexed array
+//        that is in the hashmap. If it finds it, it returns it.
+//     4. If the loop ends, then the integer is the maximum + 1.
