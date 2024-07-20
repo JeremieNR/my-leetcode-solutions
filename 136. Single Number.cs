f2f0477766a602d8eed4624525a5ba54b257c71f
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-07-20 11:38:09 -0400

    Added 136. Single Number.cs

---
+++ 136. Single Number.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        Dictionary<int, int> hm = new Dictionary<int, int>();
+        for (int x = 0; x < nums.Length; x++) {
+            if (hm.ContainsKey(nums[x])) {
+                hm.Remove(nums[x]);
+            } else {
+                hm[nums[x]] = x;
+            }
+        }
+        return (new List<int>(hm.Keys))[0];
+    }
+}
+
+
+// The overall time complexity is O(n), where n represents the number of elements in the input array.
+// 
+// The process I went through here is that I knew I could use a hashmap to store integers and check
+// for duplicates, making the overall time complexity O(n). Here is the breakdown of my code:
+//     1. I start my function by getting the standard hashmap and initializing the mutable hashmap as hm.
+//     2. Then I open a for-loop and use an if-else statement to see if the key already exists 
+//        in my hashmap. If it does, it removes it, and if it doesn't, it adds it.
+//     3. Then I return the only key stored in the hashmap.
