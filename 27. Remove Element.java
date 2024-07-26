f1606c1865abb10262876d11f8c39b6686c968a4
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-07-26 08:56:59 -0400

    Added 27. Remove Element.java

---
+++ 27. Remove Element.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        List<Integer> out = new ArrayList<Integer>();
+        for (int x = 0; x < nums.length; x++) {
+            if (nums[x] != val) {
+                out.add(nums[x]);
+            }
+        }
+        for (int x = 0; x < nums.length; x++) {
+            if (x < out.size()) {
+                nums[x] = out.get(x);
+            }
+        }
+        return out.size();
+    }
+}
+
+
+// The overall time complexity is O(n), where n is the number of elements in the input array. 
+// 
+// The process I went through here is knew I could simply add the filtered elements to another array and later add it back to the original,
+// which ended up making the overall time complexity O(n). Here is the breakdown of my code:
+//     1. I start my function by initializing my temporary array called out.
+//     2. Then I open a for-loop which indexes values in the temporary array out if they're not the val integer.
+//     3. Then I open another for-loop which replaces all elements in nums to the correct output, which is an ordered non-duplicate
+//        array of integers with the end of the array typically being artifact integers from the original array.
+//     4. I then return the length of the out array (as per the question's demand) which is the length of the non-val integers in the 
+//        nums array. 
