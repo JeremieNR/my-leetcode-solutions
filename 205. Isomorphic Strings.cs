3e74f8c81947ae8ea07ffa4086c72789fe298061
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-07-28 16:00:13 -0400

    Added 205. Isomorphic Strings.cs

---
+++ 205. Isomorphic Strings.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public bool IsIsomorphic(string s, string t) {
+        IDictionary<int, int> hm1 = new Dictionary<int, int>();
+        IDictionary<int, int> hm2 = new Dictionary<int, int>();
+        for (int x = 0; x < s.Length; x++) {
+            if (((hm1.ContainsKey(t[x])) && (s[x] != hm1[t[x]])) || ((hm2.ContainsKey(s[x])) && (t[x] != hm2[s[x]]))) {
+                return false;
+            } else {
+                hm2[s[x]] = t[x];
+                hm1[t[x]] = s[x];
+            }
+        }
+        return true;
+    }
+}
+
+
+// The overall time complexity is O(n), where n represents the length of the input strings.
+// 
+// The process I went through here is that I knew I could use a hashmap to store character data from one string to the other, 
+// making the overall time complexity O(n). Here is the breakdown of my code:
+//     1. I start my function by initializing both hashmaps.
+//     2. Then I open a for-loop which checks if a character comparison is wrong in either hashmaps. If so, returns False. The
+//        for-loop also indexes characters if they don't exist in the hashmaps.
+//     3. If the for-loop ends, True is returned.
