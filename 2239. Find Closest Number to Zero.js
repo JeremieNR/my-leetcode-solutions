d9720f1c6eb1d3a72b835c8db5f4d651743599ce
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-07-30 10:57:35 -0400

    Added 2239. Find Closest Number to Zero.js

---
+++ 2239. Find Closest Number to Zero.js
@@ -0,0 +1,26 @@
+var findClosestNumber = function(nums) {
+    let pair = [];
+    let minimum = 10**5 + 1;
+    for (let x  = 0; x < nums.length; x++) {
+        if (Math.abs(nums[x]) < minimum) {
+            minimum = Math.abs(nums[x]);
+            pair = [nums[x], x];
+        } else if ((Math.abs(nums[x]) == minimum) && (nums[x] > pair[0])) {
+            pair = [nums[x], x];
+        }
+    }
+    return pair[0];
+};
+
+
+// The overall time complexity is O(n), where n is the number of elements in the input array. 
+// 
+// The process I went through here is that I knew I could use a list as a value-index pair for 
+// the largest number with the smallest distance to zero, making the overall time complexity O(n).
+// Here is the breakdown of my code:
+//     1. I start my function by initializing the pair list and minimum integer.
+//     2. Then, I open a for-loop to iterate through all the indexes of the array. If a new minimum 
+//        is found, the minimum integer is updated and so is the pair list. If not, if the distance 
+//        to zero is equal to the current minimum and the integer is larger than the current pair value,
+//        it will be updated.
+//     3. After the for-loop, the largest integer with the smallest distance to zero is returned.
