d5a3935393a9ca15a8a31c61b68e8f8cacbc560f
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-08-04 15:41:28 -0400

    Added 278. First Bad Version.py

---
+++ 278. First Bad Version.py
@@ -0,0 +1,20 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low = 0
+        mid = n // 2
+        while (low != mid) and (mid != n):
+            if isBadVersion(mid) == False:
+                low = mid
+            else:
+                n = mid
+            mid = (low + n) // 2
+        return mid + 1
+
+
+# The overall time complexity is O(Log(n)), where n represents the input integer n.
+# 
+# The process I went through here is that I knew I could use a binary search to find the smallest bad version,
+# making the overall time complexity O(Log(n)). Here is the breakdown of my code:
+#     1. I start my function by initializing low and mid. 
+#     2. Then I open a while-loop which will keep dividing the search area until it finds the smallest bad version.
+#     3. Then I return mid + 1 (which is the smallest bad version).
