c7f73da59c54018bc3aeeb16dc0d74b8a0d3613d
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-08-20 17:46:07 -0400

    Update 58. Length of Last Word.js

--- 58. Length of Last Word.js
+++ 58. Length of Last Word.js
@@ -1,20 +1,21 @@
 var lengthOfLastWord = function(s) {
     s = s.trim();
-    temp = "";
+    num = 0;
     for (let x = 0; x < s.length; x++) {
         if (s[s.length - 1 - x] == " ") {
-            break
+            break;
         }
-        temp += s[s.length - 1 - x];
+        num += 1;
     }
-    return temp.length;
+    return num;
 };
 
 
 // The overall time complexity is O(n), where n is the number of characters in the input string. 
 // 
-// The process I went through here is knew I could simply strip the string and split it up, and take the length of the last word,
-// which ended up making the overall time complexity O(n). Here is the breakdown of my code:
-//     1. I start my function by stripping the ends of the s input string, and initializing the temp string.
-//     2. Then I open a for-loop which adds the characters of string s backwards, and stops when it reaches a whitespace.
-//     3. Then I return the length of the temp string. 
+// The process I went through here is knew I could simply strip the string and split it up, and count the number 
+// of characters in the last word, which ended up making the overall time complexity O(n). Here is the breakdown of my code:
+//     1. I start my function by initializing my trimed string s and my counter num.
+//     2. Then I open a for-loop which adds 1 to my counter num until it reaches the white character 
+//        after the last character of the final word in s.
+//     3. Then I return num.
