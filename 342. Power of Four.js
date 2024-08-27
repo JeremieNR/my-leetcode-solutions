cbe94ea2eb066a5171766db464411df48fc65911
Jeremie Notebaert <63004366+JeremieNR@users.noreply.github.com>
2024-08-27 09:33:09 -0400

    Added 342. Power of Four.js

---
+++ 342. Power of Four.js
@@ -0,0 +1,15 @@
+var isPowerOfFour = function(n) {
+    for (let x = 0; x < 16; x++) {
+        if (4**x == n) {
+            return true;
+        }
+    }
+    return false;
+};
+
+
+// The overall time complexity is O(1). 
+// 
+// The process I went through here is that I knew I could use the maximum integer (4^16) as a loop max, making the 
+// overall time complexity O(1). I start my function by openning a for-loop, which checks if x is any power of 4. 
+// If it is, it returns true, and if the for-loop ends, it means it isn't and thus returns false.
